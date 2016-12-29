4c6736fc3a32e6038414e1780acddfd0fad82dbf
Petr Shypila <petr_shypila@epam.com>
2016-12-29 18:16:12 +0100

    first commit

---
+++ models/build-graph.py
@@ -0,0 +1,14 @@
+import sys
+import numpy as np
+
+nodes = int(sys.argv[1])
+
+x = np.random.uniform(-100.0, 100.0, nodes)
+y = np.random.uniform(-100.0, 100.0, nodes)
+z = np.random.uniform(-50.0, 50.0, nodes)
+
+with open('brain_vertex_low.obj', 'w') as f:
+    f.write("g brain\n")
+
+    for n in np.arange(nodes):
+        f.write("v {:.2f} {:.2f} {:.2f}\n".format(x[n], y[n], z[n]))
