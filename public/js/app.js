2c5bfaa576dfb80aa65dd2d8dc0e4347011cffec
Petr Shypila <ikrumping@gmail.com>
2017-01-05 23:19:30 +0100

    v2 init

---
+++ public/js/app.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+	particlesJS.load('particles-js', 'model/particles.json', function() {
+	  console.log('callback - particles.js config loaded');
+	});
+};
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var static = require('node-static');
 //
 // Create a node-static server instance to serve the './public' folder
 //
-var file = new static.Server('./');
+var file = new static.Server('./public');
 
 var port_ = (process.env.PORT || 8080);
 
